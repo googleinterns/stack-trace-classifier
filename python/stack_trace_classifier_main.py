745a81a64c9bde00a370b87e8d7ac385e4401d85
mattxu-intern <mattxu@google.com>
2020-07-27 20:44:11 +0000

    changed main file structure to match abseil examples (#10)
    refactored code into a method

--- python/stack_trace_classifier_main.py
+++ python/stack_trace_classifier_main.py
@@ -1,6 +1,4 @@
 """Demo module for running classification algorithms and summarizer."""
-import argparse
-
 from error_code_matcher import ErrorCodeMatcher
 from k_means_clusterer import KMeansClusterer
 import pandas_gbq
@@ -8,11 +6,13 @@ import proto.big_query_config_pb2
 import proto.config_pb2
 from summarizer import Summarizer
 
+from absl import app
+from absl import flags
 from google.cloud import bigquery
 from google.protobuf import text_format
 
 
-def get_input_dataframe_table(project_id, dataset_id, input_table_id):
+def get_input_dataframe_table(project_id, dataset_id, input_table_id, client):
   """Attempts to get an input dataframe utilizing the bigquery configurations.
 
   Args:
@@ -52,57 +52,70 @@ def output_dataframe_to_gbq(output_dataframe, project_id, dataset_id,
                     project_id=project_id)
 
 
-if __name__ == "__main__":
-  # future CLI args can go here following the same format
-  arg_parser = argparse.ArgumentParser()
-  arg_parser.add_argument(
-      '--config',
-      nargs=1,
-      type=str,
-      required=True,
-      help=
-      'configuration file path, expected to be in the format as outlined in config.proto'
-  )
-  arg_parser.add_argument(
-      '--bigquery_config',
-      nargs=1,
-      type=str,
-      help=
-      'bigquery configuration file path to pass in expected to be in format of big_query_config.proto'
-  )
+def run_classification_summary(df, classifier_config):
+  """Runs the various classification algorithms outputting a summary dataframe.
 
-  args = arg_parser.parse_args()
+  Args:
+    df: input dataframe containing the error information we wish to classify and summarize
 
+    classifier_config: configuration file expected to be in the format of config.proto
+
+  Returns:
+    pandas dataframe that summarizes the information obtained from the classification algorithms
+      run on the input dataframe
+  """
+  # Running our classifiers
+  error_code_matcher = ErrorCodeMatcher(df, classifier_config)
+  error_code_matcher.match_informative_errors()
+  k_means_classifier = KMeansClusterer(df, classifier_config)
+  k_means_classifier.cluster_errors()
+
+  # Running the summarizer
+  summarizer = Summarizer(df, classifier_config)
+  return summarizer.generate_summary()
+
+
+FLAGS = flags.FLAGS
+flags.DEFINE_string(
+    'config', None,
+    'configuration file path, expected to be in the format as outlined by config.proto'
+)
+flags.DEFINE_string(
+    'big_query_config', None,
+    'big query configuration file path to pass in expected to be in format of big_query_config.proto'
+)
+flags.mark_flag_as_required('config')
+# future flag arguments, i.e. plx workflow client, can go here
+
+
+def main(argv):
   # Read classifier configurations from proto file passed in
-  classifier_config_path = args.config[0]
+  classifier_config_path = FLAGS.config
   classifier_config = proto.config_pb2.Config()
   classifier_file = open(classifier_config_path, 'r')
   classifier_config = text_format.Parse(classifier_file.read(),
                                         classifier_config)
 
-  if args.bigquery_config:
+  if FLAGS.big_query_config:
     # Personal Client, YMMV.
     # In the future, change this to plx workflow client, or BQ service agent
     client = bigquery.Client()
     # Read BQ configurations from proto file passed in
-    bigquery_config_path = args.bigquery_config[0]
-    bigquery_config = proto.big_query_config_pb2.BigQueryConfig()
-    bigquery_file = open(bigquery_config_path, 'r')
-    bigquery_config = text_format.Parse(bigquery_file.read(), bigquery_config)
+    big_query_config_path = FLAGS.big_query_config
+    big_query_config = proto.big_query_config_pb2.BigQueryConfig()
+    big_query_file = open(big_query_config_path, 'r')
+    big_query_config = text_format.Parse(big_query_file.read(),
+                                         big_query_config)
     # BigQuery Schematics
-    df = get_input_dataframe_table(bigquery_config.project_id,
-                                   bigquery_config.dataset_id,
-                                   bigquery_config.input_table_id)
+    df = get_input_dataframe_table(big_query_config.project_id,
+                                   big_query_config.dataset_id,
+                                   big_query_config.input_table_id, client)
 
-    # Running our classifiers
-    error_code_matcher = ErrorCodeMatcher(df, classifier_config)
-    error_code_matcher.match_informative_errors()
-    k_means_classifier = KMeansClusterer(df, classifier_config)
-    k_means_classifier.cluster_errors()
+    output_df = run_classification_summary(df, classifier_config)
+    output_dataframe_to_gbq(output_df, big_query_config.project_id,
+                            big_query_config.dataset_id,
+                            big_query_config.output_table_id)
 
-    # Running the summarizer
-    summarizer = Summarizer(df, classifier_config)
-    output_df = summarizer.generate_summary()
-    output_dataframe_to_gbq(output_df, bigquery_config.project_id,
-                            bigquery_config.dataset_id,
-                            bigquery_config.output_table_id)
+
+if __name__ == "__main__":
+  app.run(main)
